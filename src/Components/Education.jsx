cd083dee0d0f5ef5ee5633e6fca92e377bbcbcee
Cat <citcat21@gmail.com>
2024-05-06 08:30:47 +0000

    fixed education navigation

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Education = () => {
   return (
-    <section id="education" className="min-height">
-    <div>
-      <h1>Education</h1>
-      <p>🚧 Under construction 🚧</p>
-    </div>
+    <section id="education" className="padding">
+      <div>
+        <h1>Education</h1>
+        <p>🚧 Under construction 🚧</p>
+      </div>
     </section>
   );
-}
+};
 
 export default Education;
